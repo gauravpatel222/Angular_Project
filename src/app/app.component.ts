d3291ca93794fd5a1411d8d517657e92ef949748
Gaurav <gauravgurjar8791@gmail.com>
2025-03-24 11:55:54 +0530

    one way bindning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helloworld';
- 
+  userName:String="";
   imgUrl="https://images.squarespace-cdn.com/content/v1/60d0f750f869ef1e22c3bed9/f4395ca1-fc97-4730-b521-a144e7656ea9/Bridge+Labs.jpeg?format=1000w"
   url="bridglabz.com"
 }
